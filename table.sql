8057fb8ed6dad9d01f5d62b870e0d68c6edea3b6
shimazarei <49499236+shimazarei@users.noreply.github.com>
2020-05-03 18:00:22 +0200

    This sql file indicates the table which is created for storing data

---
+++ table.sql
@@ -0,0 +1,13 @@
+USE DB1;
+DROP TABLE node_tree_name;
+
+DROP TABLE node_tree;
+
+CREATE TABLE node_tree(iNode INT PRIMARY KEY, level INT, iLeft INT, iRight INT)ENGINE=InnoDB;
+
+CREATE TABLE node_tree_name(Id INT PRIMARY KEY AUTO_INCREMENT,iNode INT , Language VARCHAR(200),nodeName VARCHAR(200), KEY iNode(iNode), CONSTRAINT FK_id FOREIGN KEY (iNode) REFERENCES node_tree (iNode) ON DELETE CASCADE ON UPDATE CASCADE)ENGINE=InnoDB;
+
+
+
+
+
